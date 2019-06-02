4925da409f4832fda73c99f877eefd5c4c3d4d45
zhangmx123 <49980812+zhangmx123@users.noreply.github.com>
2019-06-02 22:29:38 +0800

    20190601
    我的代办的html
    用户管理的html
    人员管理的html
    人员管理的controller

--- JLDJ/Controllers/PersonManageController.cs
+++ JLDJ/Controllers/PersonManageController.cs
@@ -17,7 +17,18 @@ namespace JLDJ.Controllers
     {
         // GET: PersonManage
         private DJXTEntities2 db = new DJXTEntities2();
-
+        //[HttpGet]
+        //public JsonResult Sessionget()
+        //{
+        //    Dictionary<string, string> dic = new Dictionary<string, string>();
+        //    string username = Session["username"].ToString();
+        //    string dept = Session["dept"].ToString();
+        //    string role = Session["role"].ToString();
+        //    dic.Add("username", username);
+        //    dic.Add("dept", dept);
+        //    dic.Add("role", role);
+        //    return Json(dic, JsonRequestBehavior.AllowGet);
+        //}
         public ActionResult Index()
         {
             return View();
@@ -48,30 +59,73 @@ namespace JLDJ.Controllers
         {
             person personInfo = new person()
             {
-                    person_name = Person_Name,
-                    person_sex = Person_Sex,
-                    person_birth = Person_Birth,
-                    person_JJFZdate = Person_CJGZdate,
-                    person_CJGZdate = Person_CJGZdate,
-                    person_ZGDYdate = Person_ZGDYdate,
-                    person_YBDYdate = Person_YBDYdate,
-                    person_eduback = Person_Eduback,
-                    person_degree = Person_Degree,
-                    person_college = Person_College,
-                    person_phonenum = Person_Phonenum,
-                    person_dept = Person_Dept,
-                    person_email = Person_Email,
-                     };
-            try
-            {
+                person_name = Person_Name,
+                person_sex = Person_Sex,
+                person_birth = Person_Birth,
+                person_JJFZdate = Person_CJGZdate,
+                person_CJGZdate = Person_CJGZdate,
+                person_ZGDYdate = Person_ZGDYdate,
+                person_YBDYdate = Person_YBDYdate,
+                person_eduback = Person_Eduback,
+                person_degree = Person_Degree,
+                person_college = Person_College,
+                person_phonenum = Person_Phonenum,
+                person_dept = Person_Dept,
+                person_email = Person_Email,
+            };
+            //try
+            //{
                 db.people.Add(personInfo);
                 db.SaveChanges();
-            }
-            catch(Exception ex) {
-                throw;
-            }
+            //}
+            //catch (Exception ex) {
+            //    throw;
+            //}
+            return 1;
+        }   
+        [HttpGet]
+        public int delperson(string Person_Id)
+            {
+                person personInfo = db.people.Find(Convert.ToInt32(Person_Id));
+                if (personInfo != null)
+                {
+                    db.people.Remove(personInfo);
+                    db.SaveChanges();
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
 
+            }
+        [HttpGet]
+        public int updperson(string Person_Id,string Person_Name, string Person_Sex, string Person_Birth, string Person_CJGZdate, string Person_ZGDYdate,
+            string Person_YBDYdate, string Person_Eduback, string Person_Degree, string Person_College, string Person_Phonenum,
+            string Person_Dept, string Person_Email)
+        {
+            person personInfo = db.people.Find(Convert.ToInt32(Person_Id));
+            if (personInfo != null)
+            {
+                personInfo.person_name = Person_Name;
+                personInfo.person_sex = Person_Sex;
+                personInfo.person_birth = Person_Birth;
+                personInfo.person_CJGZdate = Person_CJGZdate;
+                personInfo.person_ZGDYdate = Person_ZGDYdate;
+                personInfo.person_YBDYdate = Person_YBDYdate;
+                personInfo.person_eduback = Person_Eduback;
+                personInfo.person_degree = Person_Degree;
+                personInfo.person_college = Person_College;
+                personInfo.person_phonenum = Person_Phonenum;
+                personInfo.person_dept = Person_Dept;
+                personInfo.person_email = Person_Email;
+                db.SaveChanges();
                 return 1;
+            }
+            else
+            {
+                return 0;
+            }
 
         }
     }
